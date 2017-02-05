7f29f8977d362172d206138a090ac05d8e989a2b
tabarry <tahir@sulata.com.pk>
2017-02-05 15:35:13 +0500

    Tried ondblcickevent

--- tmp/25n43mgl8beso.nrao4k1u1cwh.php
+++ tmp/25n43mgl8beso.nrao4k1u1cwh.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?php echo $pageInfo['title']; ?></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="./css/jquery.filer.css" rel="stylesheet">
+<head>
+    <title><?php echo $pageInfo['title']; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 
-        <!-- Jvascript -->
-        <script src="http://code.jquery.com/jquery-3.1.0.min.js" crossorigin="anonymous"></script>
+    <!-- Styles -->
+    <link href="./css/jquery.filer.css" rel="stylesheet">
 
-        <script>
-            function suSubmit(frmName, outputEle, errorEle) {
-                // Get the form.
-                var form = $('#' + frmName);
+    <!-- Jvascript -->
+    <script src="http://code.jquery.com/jquery-3.1.0.min.js" crossorigin="anonymous"></script>
 
-                // Get the messages div.
-                var formMessages = $('#' + outputEle);
+    <script>
+        function suSubmit(frmName, outputEle, errorEle) {
+            // Get the form.
+            var form = $('#' + frmName);
 
-                // Set up an event listener for the contact form.
-                $(form).submit(function(e) {
-                    // Stop the browser from submitting the form.
-                    e.preventDefault();
+            // Get the messages div.
+            var formMessages = $('#' + outputEle);
 
-                    // Serialize the form data.
-                    //////var formData = $(form).serialize();
+            // Set up an event listener for the contact form.
+            $(form).submit(function(e) {
+                // Stop the browser from submitting the form.
+                e.preventDefault();
 
-                    // Submit the form using AJAX.
-                    $.ajax({
+                // Serialize the form data.
+                //////var formData = $(form).serialize();
+
+                // Submit the form using AJAX.
+                $.ajax({
                         url: "ajax.php", // Url to which the request is send
                         type: "POST", // Type of request to be send, called as method
                         data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
@@ -39,42 +40,52 @@
                         cache: false, // To unable request pages to be cached
                         processData: false
                     })
-                            .done(function(response) {
-                                // Make sure that the formMessages div has the 'success' class.
-                                formMessages.html(response);
-                            })
-                            .fail(function(data) {
-                                alert('error');
-                            });
-
-                });
-            }
-            $(document).ready(function() {
-                suSubmit('ajax-contact', 'server-results', 'server-results');
+                    .done(function(response) {
+                        // Make sure that the formMessages div has the 'success' class.
+                        formMessages.html(response);
+                    })
+                    .fail(function(data) {
+                        alert('error');
+                    });
 
             });
+        }
+        $(document).ready(function() {
+            suSubmit('ajax-contact', 'server-results', 'server-results');
 
-        </script>
-    </head>
-    <body>
-      <div id="server-results"></div>
-        <?php echo $this->render('header.html',$this->mime,get_defined_vars(),0); ?>
-        <h1><?php echo $pageInfo['heading']; ?></h1>
-        <form id="ajax-contact" method="post" action="ajax.php" enctype="multipart/form-data">
-            <div class="field">
-                <p>Name:</p>
-                <input type="text" id="name" name="name" placeholder="Name">
-                <p>Email:</p>
-                <input type="text" id="email" name="email"  placeholder="Email">
-                <p>File:</p>
+        });
 
-                <input type="file" name="files[]" id="filer_input" multiple="multiple"/>
+        function dbl() {
+            alert('hi');
+        }
+    </script>
+</head>
 
-            </div>
+<body>
+    <div id="server-results"></div>
+    <?php echo $this->render('header.html',$this->mime,get_defined_vars(),0); ?>
+    <h1><?php echo $pageInfo['heading']; ?></h1>
+    <form id="ajax-contact" method="post" action="ajax.php" enctype="multipart/form-data">
+        <select ondblclick="dbl()">
+            <option>
+              Hello
+            </option>
+          </select>
+        <div class="field">
+            <p>Name:</p>
+            <input type="text" id="name" name="name" placeholder="Name" ondblclick="dbl()">
+            <p>Email:</p>
+            <input type="text" id="email" name="email" placeholder="Email">
+            <p>File:</p>
 
-            <div class="field">
-                <button type="submit">Send</button>
-            </div>
-        </form>
-    </body>
+            <input type="file" name="files[]" id="filer_input" multiple="multiple" />
+
+        </div>
+
+        <div class="field">
+            <button type="submit">Send</button>
+        </div>
+    </form>
+</body>
+
 </html>
